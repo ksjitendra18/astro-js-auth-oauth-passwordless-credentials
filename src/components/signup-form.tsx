bfa21bb733c8a0517ab6d6c8caaa62ad718115af
Jitendra <jitendraks1803@gmail.com>
2024-01-07 15:08:01 +0530

    credential-starter

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -5,6 +5,8 @@ import { Show, createSignal } from "solid-js";
 const SignupForm = () => {
   const [validationIssue, setValidationIssue] = createSignal("");
   const [loading, setLoading] = createSignal(false);
+  const [showPassword, setShowPassword] = createSignal(false);
+
   const handleSignup = async (e) => {
     setValidationIssue("");
     setLoading(true);
@@ -39,14 +41,14 @@ const SignupForm = () => {
           method="post"
           class="w-[100%] mx-auto md:w-auto"
         >
-          <label for="name" class="mt-5 block text-gray-600">
+          <label for="name" class="block text-gray-600">
             Name
           </label>
           <input
             type="text"
             name="name"
             id="name"
-            class="border-slate-400 px-3 w-full md:w-[400px] py-2 rounded-md border-2"
+            class="border-slate-600 px-3 w-full md:w-[400px] py-2 rounded-md border-2"
           />
           <label for="email" class="mt-5 block text-gray-600">
             Email
@@ -56,7 +58,7 @@ const SignupForm = () => {
             name="email"
             id="email"
             required
-            class="border-slate-400 px-3 w-full md:w-[400px] py-2 rounded-md border-2"
+            class="border-slate-600 px-3 w-full md:w-[400px] py-2 rounded-md border-2"
           />
           <label for="password" class="mt-5 block text-gray-600">
             Password
@@ -67,23 +69,26 @@ const SignupForm = () => {
               name="password"
               id="password"
               required
-              class="border-slate-400 px-3 w-full md:w-[400px] py-2 rounded-md border-2"
+              class="border-slate-600 px-3 w-full md:w-[400px] py-2 rounded-md border-2"
             />
-            <button type="button" aria-label="Password Invisible.">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-6 select-none text-gray-700 cursor-pointer h-6 absolute top-2 right-2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                ></path>
-              </svg>
+            <button
+              onClick={() => setShowPassword((prev) => !prev)}
+              type="button"
+              aria-label="Password Invisible."
+            >
+              {showPassword() ? (
+                <img
+                  src="/eye-icon.svg"
+                  class="w-6 select-none text-gray-700 cursor-pointer h-6 absolute top-2 right-2"
+                  width="20px"
+                />
+              ) : (
+                <img
+                  src="/eye-close.svg"
+                  class="w-6 select-none text-gray-700 cursor-pointer h-6 absolute top-2 right-2"
+                  width="20px"
+                />
+              )}
             </button>
           </div>
 
