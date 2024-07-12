5309ae4638ca84540e16f617a92e412d898b6b45
ksjitendra18 <jitendraks1803@gmail.com>
2024-07-12 21:42:52 +0530

    update packages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "drizzle-kit";
 
-export default {
+if (!process.env.DB_URL || !process.env.DB_TOKEN) {
+  throw new Error("DB_URL and DB_TOKEN missing");
+}
+
+export default defineConfig({
   dialect: "sqlite",
   driver: "turso",
   schema: "./src/db/schema.ts",
@@ -9,4 +13,4 @@ export default {
     url: process.env.DB_URL!,
     authToken: process.env.DB_TOKEN!,
   },
-};
+});
