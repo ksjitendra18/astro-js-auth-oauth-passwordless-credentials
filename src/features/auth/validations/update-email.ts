5de652c2867e3ff42b309394b6074d01d7650ec4
Jitendra <jitendraks1803@gmail.com>
2025-06-14 11:54:16 +0530

    migrate to zod v4

--- src/features/auth/validations/update-email.ts
+++ src/features/auth/validations/update-email.ts
@@ -1,17 +1,13 @@
-import { object, string, z } from "zod/v4";
+import { email, object, string, z } from "zod/v4";
 
 export const UpdateEmailSchema = object({
-  currentEmail: string({ error: "Current Email is required" }).email({
-    message: "Enter a valid email",
-  }),
+  currentEmail: email({ error: "Current Email is required" }),
 
   currentEmailOtp: string({
     error: "Current email OTP is required",
   }).min(6, "OTP should be of 6 digits"),
 
-  newEmail: string({ error: "New Email is required" }).email({
-    message: "Enter a valid email",
-  }),
+  newEmail: email({ error: "New Email is required" }),
 
   newEmailOtp: string({ error: "New email OTP is required" }).min(
     6,
